0aab3517e17c515034e03de667a954f7d56d6604
zadigg <abelkibebe@gmail.com>
2024-06-02 22:51:10 -0500

    chore: add vercel monitoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MainComponent from "./component/MainComponent";
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
       <div className="">
           {/*<RegisteredUsers />*/}
         <MainComponent />
+          <SpeedInsights />
 
       </div>
   );
