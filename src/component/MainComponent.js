ecbf243c7ddae81b392598d2850e6e953056e1ce
zadigg <abelkibebe@gmail.com>
2024-08-11 20:30:53 -0500

    chore: change background

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import ProjectJailBreakImage from '../asset/ProjectJailBreak.webp'; // Ensure this path is correct
-import ProjectJailBreakImageTwo from '../asset/ProjectJailBreak1.webp'; // Ensure this path is correct
+// import ProjectJailBreakImage from '../asset/ProjectJailBreak.webp';
+// import ProjectJailBreakImageTwo from '../asset/ProjectJailBreak1.webp';
+import ProjectJailBreakImageThree from '../asset/ProjectJailBreak2.webp';
 import RegisterForm from './RegisterForm';
 import ParticipantsList from './ParticipantsList';
 import { fetchRegisteredCount, fetchParticipants, registerParticipant } from '../api';
@@ -73,7 +74,7 @@ const MainComponent = () => {
 
     return (
         <div className="bg-white dark:bg-gray-900 flex">
-            <div className="hidden lg:block lg:w-2/3 bg-cover" style={{ backgroundImage: `url(${ProjectJailBreakImageTwo})` }}>
+            <div className="hidden lg:block lg:w-2/3 bg-cover" style={{ backgroundImage: `url(${ProjectJailBreakImageThree})` }}>
             </div>
             <div className="w-full max-w-md px-6 mx-auto lg:w-1/3 mt-14">
                 <div className="bg-gray-50 px-5 py-3 dark:bg-gray-800 flex justify-between">
